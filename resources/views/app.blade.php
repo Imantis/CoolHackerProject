24b738060c528c4b01d4073917682ad1ee98bc6f
Imant <mrimant@gmail.com>
2018-12-05 21:17:06 +0200

    ADD style for social button and links

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -52,19 +52,22 @@
 <div style="clear:both;"></div>
 <footer class="footer-distributed">
 
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <div class="footer-right">
 
-	<a href="#"><i class="fa fa-facebook"></i></a>
-	<a href="#"><i class="fa fa-twitter"></i></a>
-	<a href="#"><i class="fa fa-linkedin"></i></a>
-	<a href="#"><i class="fa fa-github"></i></a>
+	<a href="https://www.facebook.com/zuck"><i class="fa fa-facebook"></i></a>
+	<a href="https://twitter.com/search?q=elon+musk&ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Esearch"><i class="fa fa-twitter"></i></a>
+	<a href="https://www.linkedin.com/in/imant-lizinskis-3a75b2165/"><i class="fa fa-linkedin"></i></a>
+	<a href="https://github.com/vados169/CoolHackerProject"><i class="fa fa-github"></i></a>
 
 </div>
 
 <div class="footer-left">
 
 	<p class="footer-links">
-		<a href="#">Home</a>
+		<a href="/list">Home</a>
 		·
 		<a href="#">Blog</a>
 		·
